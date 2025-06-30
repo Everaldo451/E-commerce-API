51124f91092b695ef7c7e8fe0d4b19b7d5e5c3f7
Everaldo451 <everaldo.work.jr@gmail.com>
2025-06-30 16:32:02 -0300

    update users viewset and add me route

--- src/users/views.py
+++ src/users/views.py
@@ -1,15 +1,28 @@
 from rest_framework import viewsets, permissions
-from rest_framework.request import Request
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 from .models import User
 from .serializer import UserSerializer
 
-class IsAdminOrPostMethod(permissions.BasePermission):
-    def has_permission(self, request:Request, view):
-        return (request.user.is_authenticated and request.user.is_staff) or request.method=="POST"
+class IsNotAuthenticatedAndPostMethod(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return not request.user.is_authenticated and request.method=="POST"
+    
+
+class IsCurrentUserOrAdmin(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_staff or obj == request.user
     
 
 class UserViewSet(viewsets.ModelViewSet):
-
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (IsAdminOrPostMethod,)
+    permission_classes = (
+        (permissions.IsAuthenticated & IsCurrentUserOrAdmin) | IsNotAuthenticatedAndPostMethod,
+    )
+
+    @action(detail=False, methods=["GET"], permission_classes=(permissions.IsAuthenticated,))
+    def me(self, request):
+        serializer = self.get_serializer(request.user)
+        return Response(serializer.data)
