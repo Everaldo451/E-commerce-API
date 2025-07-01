eb1e5b4993ce5a9d0fde3a6f402dec9ff03f3b49
Everaldo451 <everaldo.work.jr@gmail.com>
2025-07-01 19:35:18 -0300

    add search method to ProductViewSet

--- src/products/serializer.py
+++ src/products/serializer.py
@@ -19,26 +19,22 @@ class ProductMediaSerializer(serializers.ModelSerializer):
         
 
 class ProductSerializer(serializers.ModelSerializer):
-    tags = serializers.SlugRelatedField(
-        many=True,
-        slug_field='name',
-        queryset=Tag.objects.all()
-    )
+    tags = TagSerializer(many=True)
     media = ProductMediaSerializer(many=True)
-    created_by = serializers.SerializerMethodField('get_created_by')
-
-    def get_created_by(self, obj):
-        return f'{obj.created_by.first_name} {obj.created_by.last_name}'
+    created_by = serializers.ReadOnlyField(source="created_by.username")
 
     class Meta:
         model = Product
-        fields = ['id', 'name', 'price', 'tags', 'media', 'created_at', 'is_available','created_by']
+        fields = ['id', 'name', 'price', 'stock', 'tags', 'media', 'created_at', 'is_available', 'created_by']
         read_only_fields = ['id', 'created_at', 'is_available', 'created_by']
     
     def use_product_media_method(self, *args, media_data:list[dict], product, media_method:Callable) -> None:
         for media in media_data:
                 media_data = {key: media[key] for key in args}
                 media_method(product=product, **media_data)
+
+    def get_or_create_tags(self, tags_data):
+        return [Tag.objects.get_or_create(**tag)[0] for tag in tags_data]
 
     def create(self, validated_data:dict):
         request = self.context.get('request')
@@ -49,7 +45,8 @@ class ProductSerializer(serializers.ModelSerializer):
 
         product = Product.objects.create(**validated_data, created_by=user)
 
-        product.tags.set(tags_data)
+        tags = self.get_or_create_tags(tags_data)
+        product.tags.set(tags)
         self.use_product_media_method(
             "data",
             media_data=media_data, 
@@ -67,10 +64,11 @@ class ProductSerializer(serializers.ModelSerializer):
         instance.price = validated_data.get('price', instance.price)
 
         if tags_data is not None:
+            tags = self.get_or_create_tags(tags_data)
             if request.method == "PATCH":
-                instance.tags.add(*tags_data)
+                instance.tags.add(*tags)
             else:
-                instance.tags.set(tags_data)
+                instance.tags.set(tags)
 
         instance.save()
 
@@ -82,3 +80,17 @@ class ProductSerializer(serializers.ModelSerializer):
                 media_method=ProductMedia.objects.get_or_create,
             )
         return instance
+
+
+class SearchSerializer(serializers.ModelSerializer):
+    tags = serializers.SlugRelatedField(
+        many=True,
+        slug_field='name',
+        queryset=Tag.objects.all()
+    )
+    created_by = serializers.CharField(source="created_by.username")
+
+    class Meta:
+        model = Product
+        fields = ['name', 'tags', 'created_at', 'created_by']
+
--- src/products/views.py
+++ src/products/views.py
@@ -1,6 +1,11 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from drf_yasg.utils import swagger_auto_schema
+
 from .models import Product
-from .serializer import ProductSerializer
+from .serializer import ProductSerializer, SearchSerializer
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
@@ -13,3 +18,12 @@ class ProductViewsets(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes=(permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
+
+    @action(detail=False, methods=["GET"])
+    def search(self, request:Request):
+        serializer = SearchSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        products = Product.objects.filter(**serializer.validated_data).all()
+        serializer = self.get_serializer(products, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
