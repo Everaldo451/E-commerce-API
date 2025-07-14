756a5dd6b342c63e49509b870ab682de5133844c
Everaldo451 <everaldo.work.jr@gmail.com>
2025-07-14 19:36:14 -0300

    add enums file

---
+++ src/backend/core/enums.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class OrderStatus(Enum):
+    PENDING = 'PENDING'
+    PAID = 'PAID'
+    PROCESSING = 'PROCESSING'
+    SHIPPED = 'SHIPPED'
+    DELIVERED = 'DELIVERED'
+    CANCELED = 'CANCELED'
