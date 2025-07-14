5c20bf340cd3aaa77abc70639e56205d8a3321b5
Everaldo451 <everaldo.work.jr@gmail.com>
2025-07-14 20:13:03 -0300

    add IsAdminOrOwner permission

--- src/backend/core/permissions/__init__.py
+++ src/backend/core/permissions/__init__.py
@@ -14,6 +14,16 @@ class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.created_by == request.user
     
+
+class IsAdminOrOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return (
+            IsOwner().has_object_permission(request, view, obj) 
+            or 
+            permissions.IsAdminUser().has_object_permission(request, view, obj)
+        )
+
+
 class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
