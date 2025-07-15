cadabcaa0676454256b46984eaaa28e45be39003
Everaldo451 <everaldo.work.jr@gmail.com>
2025-07-14 21:18:32 -0300

    update search route

--- src/products/views.py
+++ src/products/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets, permissions, status
+from django.db import OperationalError
+
+from rest_framework import viewsets, permissions, status, serializers
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.decorators import action
@@ -23,9 +25,26 @@ class ProductViewsets(viewsets.ModelViewSet):
     )
     @action(detail=False, methods=["GET"])
     def search(self, request:Request):
-        serializer = SearchSerializer(data=request.data)
+        serializer = SearchSerializer(data=request.query_params)
         serializer.is_valid(raise_exception=True)
 
-        products = Product.objects.filter(**serializer.validated_data).all()
-        serializer = self.get_serializer(products, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        filters = serializer.validated_data.copy()
+        keys_to_update = []
+
+        for key in filters:
+            if isinstance(serializer.fields[key], serializers.ManyRelatedField):
+                keys_to_update.append(key)
+
+        for key in keys_to_update:
+            filters[f'{key}__in'] = filters.pop(key)
+
+        try:
+            products = Product.objects.filter(**filters).all()
+            serializer = self.get_serializer(products, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except OperationalError:
+            return Response(
+                {"detail": "Database connection error."},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
